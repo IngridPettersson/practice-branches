2810241de2cf752b9054f623f4f3a42bad1ac71b
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-03-02 14:43:48 +0100

    Add this awesome feature

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <p>
+                A paragraph
+            </p>
+        </div>
+    )
+}
+
+
+
+export default App;
